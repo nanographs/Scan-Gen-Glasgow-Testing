ae1ba7885b0c952f5cd55bcffe6b6af4f95a98ae
isabelburgos <isabelmburgos01@gmail.com>
2023-07-06 12:59:54 -0700

    recent changes to gui, etc

---
+++ software/glasgow/applet/video/scan_gen/output_formats/pg_gui_with_more.py
@@ -0,0 +1,113 @@
+from time import perf_counter
+
+import numpy as np
+
+from PyQt6 import QtWidgets
+
+import pyqtgraph as pg
+from pyqtgraph.exporters import Exporter
+from pyqtgraph.Qt import QtCore
+
+app = pg.mkQApp("Scan Live View")
+
+settings = np.loadtxt("/Users/isabelburgos/glasgow_env/Scan-Gen-Glasgow-Testing/Scan Capture/current_display_setting")
+dimension = int(settings)
+
+## Define a top-level widget to hold everything
+w = QtWidgets.QWidget()
+w.setWindowTitle('/|/|/|/| scanning /|/|/|/|')
+
+## Create a grid layout to manage the widgets size and position
+layout = QtWidgets.QGridLayout()
+w.setLayout(layout)
+
+## Create window with GraphicsView widget
+win = pg.GraphicsLayoutWidget()
+#win.show()  ## show widget alone in its own window
+
+# A plot area (ViewBox + axes) for displaying the image
+#view = win.addPlot(title="")
+view = win.addViewBox()
+
+## lock the aspect ratio so pixels are always square
+view.setAspectLocked(True)
+
+## Create image item
+img = pg.ImageItem(border='w')
+view.addItem(img)
+
+## Set initial view bounds
+view.setRange(QtCore.QRectF(0, 0, dimension, dimension))
+
+# btn = QtWidgets.QPushButton('press me')
+# exporter = Exporter(img)
+# def button_action():
+#     print("button")
+#     exporter.export()
+# btn.clicked.connect(button_action)
+
+## add widgets to layout
+#layout.addWidget(btn,0,0)
+layout.addWidget(win,0,1)
+w.show()
+
+updateTime = perf_counter()
+elapsed = 0
+
+timer = QtCore.QTimer()
+timer.setSingleShot(True)
+# not using QTimer.singleShot() because of persistence on PyQt. see PR #1605
+
+
+
+def updateData():
+    global img, updateTime, elapsed
+
+    data = np.memmap("/Users/isabelburgos/glasgow_env/Scan-Gen-Glasgow-Testing/Scan Capture/current_frame",
+    shape = (dimension,dimension))
+
+    #print(data)
+    #print(data.shape)
+    img.setImage(np.rot90(data,k=3)) #this is the correct orientation to display the image
+    
+
+    timer.start(1)
+    now = perf_counter()
+    elapsed_now = now - updateTime
+    updateTime = now
+    elapsed = elapsed * 0.9 + elapsed_now * 0.1
+
+    #print(f"{1 / elapsed:.1f} fps")
+    
+timer.timeout.connect(updateData)
+updateData()
+
+# Custom ROI for selecting an image region
+# roi = pg.ROI([10,100], [100, 500])
+# roi.addScaleHandle([0.5, 1], [0.5, 0.5])
+# roi.addScaleHandle([0, 0.5], [0.5, 0.5])
+# view.addItem(roi)
+# roi.setZValue(10)  # make sure ROI is drawn above image
+
+
+
+
+# Isocurve drawing
+iso = pg.IsocurveItem(level=0.8, pen='g')
+iso.setParentItem(img)
+iso.setZValue(5)
+
+# Contrast/color control
+hist = pg.HistogramLUTItem()
+hist.setImageItem(img)
+win.addItem(hist)
+
+# Draggable line for setting isocurve level
+isoLine = pg.InfiniteLine(angle=0, movable=True, pen='g')
+hist.vb.addItem(isoLine)
+hist.vb.setMouseEnabled(y=False) # makes user interaction a little easier
+isoLine.setValue(0.8)
+isoLine.setZValue(1000) # bring iso line above contrast controls
+
+if __name__ == '__main__':
+    pg.exec()
