c232ba0f79be78c259d82c821a886318686a0dd8
isabelburgos <isabelmburgos01@gmail.com>
2023-07-11 15:08:22 -0700

    working test bitstream to do averaging on

--- software/glasgow/applet/video/scan_gen/scan_gen_components/pixel_averaging.py
+++ software/glasgow/applet/video/scan_gen/scan_gen_components/pixel_averaging.py
@@ -13,7 +13,7 @@ height, width = image.shape
 
 image_container = np.zeros(image.shape)
 
-image_bits = []
+# image_bits = []
 image_bytes = []
 for i in range(height):
     for j in range(width):
@@ -22,27 +22,30 @@ for i in range(height):
         variation = np.random.randint(1,10,10)
         n_stack_varied = np.add(n_stack,variation)
         image_bytes += n_stack_varied.tolist()
-        for n in n_stack_varied:
-            n_binary = '{0:14b}'.format(round(n))
-            image_bits.append(n_binary)
-image_bits_ = iter(image_bits)
+        # for n in n_stack_varied:
+        #     n_binary = '{0:08b}'.format(n)
+        #     image_bits.append(n_binary)
+#image_bits_ = iter(image_bits)
+image_bytes_ = iter(image_bytes)
+
+
 
 
 class Pixel_Average(Elaboratable):
     def __init__(self):
-        self.in_pixel = Signal(14)
+        self.in_pixel = Signal(8)
         self.s = Signal()
     def elaborate(self,platform):
         m = Module()
         m.d.sync += self.s.eq(1)
-        m.d.sync += self.in_pixel.eq(0)
+        m.d.comb += self.in_pixel.eq(0)
         return m
 
 def test():
     dut = Pixel_Average()
     def bench():
-        for _ in range(20):
-            yield dut.in_pixel.eq(Const(next(image_bits_)))
+        for _ in range(200):
+            yield dut.in_pixel.eq(Const(next(image_bytes_),unsigned(8)))
             yield
     sim = Simulator(dut)
     sim.add_clock(1e-6) # 1 MHz
@@ -50,5 +53,5 @@ def test():
     with sim.write_vcd("scan_sim_average.vcd"):
         sim.run()
 
-# if __name__ == "__main__":
-#     test()
+if __name__ == "__main__":
+    test()
