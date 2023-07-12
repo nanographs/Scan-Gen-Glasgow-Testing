101d7a22cc2dfb82a69c79fcf56f996645c186e0
isabelburgos <isabelmburgos01@gmail.com>
2023-07-11 18:24:29 -0700

    mostly working averaging module

--- software/glasgow/applet/video/scan_gen/scan_gen_components/pixel_averaging.py
+++ software/glasgow/applet/video/scan_gen/scan_gen_components/pixel_averaging.py
@@ -28,36 +28,95 @@ for i in range(height):
 #image_bits_ = iter(image_bits)
 image_bytes_ = iter(image_bytes)
 
+EXTERNAL = 0x00
+WRITE = 0x01
+CALCULATE = 0x02
+INCREMENT = 0x03
+SHUFFLE = 0x04
 
 class Pixel_Average(Elaboratable):
     def __init__(self):
         self.running_average = Signal(15)
-        self.in_pixel = Signal(14)
-        self.current_pixel = Signal(14)
+        self.average_output = Signal(14)
+        
+        self.P1 = Signal(14)
+        self.P2 = Signal(14)
         self.dwell_time = 10
         self.ovf = Signal()
+        self.state = Signal(8)
     def elaborate(self,platform):
         m = Module()
+        self.in_pixel = Signal(14)
         m.submodules.dwell_ctr = dwell_ctr = RampGenerator(self.dwell_time)
-        m.d.comb += [self.in_pixel.eq(0), dwell_ctr.en.eq(1)]
-        m.d.sync += [
-        self.current_pixel.eq(self.in_pixel)]
-        with m.If(dwell_ctr.ovf):
-            m.d.comb += self.running_average.eq(0)
-        with m.Else():
-            m.d.comb += self.running_average.eq((self.running_average + self.current_pixel)//2)
+        m.d.comb += [dwell_ctr.en.eq(0)]
+        # m.d.sync += [
+        # self.current_pixel.eq(self.in_pixel)]
+        # with m.If(dwell_ctr.ovf):
+        #     m.d.comb += self.running_average.eq(0)
+        # with m.Else():
+        #     m.d.comb += self.running_average.eq((self.running_average + self.current_pixel)//2)
+
+        with m.FSM():
+            with m.State("External"):
+                m.d.comb += self.state.eq(EXTERNAL)
+                m.next = "Write"
+            with m.State("Write"):
+                m.d.comb += self.state.eq(WRITE)
+                m.d.sync += [self.P1.eq(self.in_pixel)]
+                with m.If(self.P2):
+                    m.next = "Calculate"
+                with m.Else():
+                    m.d.sync += [self.running_average.eq(self.P1)]
+                    m.next = "Increment"
+            with m.State("Calculate"):
+                m.d.comb += self.state.eq(CALCULATE)
+                m.d.sync += self.running_average.eq((self.P1 + self.P2)//2)
+                m.next = "Increment"
+            with m.State("Increment"):
+                m.d.comb += self.state.eq(INCREMENT)
+                m.d.comb += dwell_ctr.en.eq(1)
+                m.next = "Shuffle"
+            with m.State("Shuffle"):
+                m.d.comb += self.state.eq(SHUFFLE)
+                with m.If(dwell_ctr.ovf):
+                    m.d.sync += [self.average_output.eq(self.running_average),
+                    self.P2.eq(0)]
+                with m.Else():
+                    m.d.sync += self.P2.eq(self.running_average)
+                m.next = "External"
+
         return m
     def ports(self):
         return[self.submodules.dwell_ctr, self.ovf, self.running_average, 
-        self.in_pixel, self.current_pixel]
+        self.in_pixel, self.P1, self.P2, self.state, self.average_output]
 
 def test():
     
     dut = Pixel_Average()
     def bench():
-        for _ in range(200):
-            yield dut.in_pixel.eq(Const(next(image_bytes_),unsigned(14)))
+        ## these assertions never fail so i don't think they're actually being checked
+        ## but at least this produces a good simulation
+        for n in range(3):
+            assert dut.P1.eq(1)
+            next_pixel = Const(next(image_bytes_),unsigned(14))
+            yield dut.in_pixel.eq(next_pixel)
+            assert dut.state.eq(WRITE)
+            assert dut.P1.eq(next_pixel)
             yield
+            assert dut.state.eq(INCREMENT)
+            yield
+            assert dut.state.eq(SHUFFLE)
+            for n in range(dut.dwell_time):
+                yield
+                assert dut.state.eq(EXTERNAL)
+                yield dut.in_pixel.eq(Const(next(image_bytes_),unsigned(14)))
+                assert dut.state.eq(WRITE)
+                yield
+                assert dut.state.eq(CALCULATE)
+                yield
+                assert dut.state.eq(INCREMENT)
+                yield
+                assert dut.state.eq(SHUFFLE)
     sim = Simulator(dut)
     sim.add_clock(1e-6) # 1 MHz
     sim.add_sync_process(bench)
