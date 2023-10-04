9ae9a4ce8e9ddd49292fbc3da13d1ced78c1f498
isabelburgos <isabelmburgos01@gmail.com>
2023-10-04 14:00:19 -0700

    toggling pattern mode from GUI works

--- software/glasgow/applet/video/scan_gen/output_formats/brand_new_gui.py
+++ software/glasgow/applet/video/scan_gen/output_formats/brand_new_gui.py
@@ -9,7 +9,9 @@ import os, datetime
 
 import numpy as np
 
-from PyQt6.QtWidgets import QMainWindow, QPushButton
+from PyQt6.QtWidgets import (QMainWindow, QPushButton, 
+                            QGridLayout, QWidget, QComboBox, 
+                            QLabel, QSpinBox, QFileDialog)
 
 import pyqtgraph as pg
 from pyqtgraph.dockarea import DockArea, Dock
@@ -36,26 +38,235 @@ cwd = os.getcwd()
 
 scan_controller = ScanController()
 
-class MainWindow(QMainWindow):
+class ResolutionDropdown(QGridLayout):
+    def __init__(self):
+        super().__init__()
+        self.menu = QComboBox()
+        self.label = QLabel("Resolution")
+        self.menu.addItem('512x512')
+        self.menu.addItem('1024x1024')
+        self.menu.addItem('2048x2048')
+        self.menu.addItem('4096x4096')
+        self.menu.addItem('8192x8192')
+        self.menu.addItem('16384x16384')
+        self.addWidget(self.label,0,1)
+        self.addWidget(self.menu,1,1)
+
+class DwellTimeSelector(QGridLayout):
+    def __init__(self):
+        super().__init__()
+        self.spinbox = QSpinBox()
+        self.label = QLabel("Dwell Time")
+        self.spinbox.setRange(1,255)
+        self.spinbox.setSingleStep(1)
+        self.addWidget(self.label,0,1)
+        self.addWidget(self.spinbox,1,1)
+
+class ImportPatternFileWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Import Pattern File")
+        self.layout = QGridLayout()
+        self.setLayout(self.layout)
+
+        self.file_dialog = QFileDialog()
+        self.file_dialog.setNameFilter("tr(Images (*.bmp)")
+        self.layout.addWidget(self.file_dialog)
+
+
+
+class MainWindow(QWidget):
 
     def __init__(self):
         super().__init__()
+        self.dimension = scan_controller.dimension
 
         self.setWindowTitle("Scan Control")
+        self.layout = QGridLayout()
+        self.setLayout(self.layout)
+
+
         self.conn_btn = QPushButton("Disconnected")
+        self.conn_btn.setCheckable(True) 
         self.conn_btn.clicked.connect(self.connect)
 
-        # Set the central widget of the Window.
-        self.setCentralWidget(self.conn_btn)
-    
+        self.mode_select_dropdown = QComboBox()
+        self.mode_select_dropdown.addItem("Imaging")
+        self.mode_select_dropdown.addItem("Patterning")
+        self.mode_select_dropdown.currentIndexChanged.connect(self.mode_select)
+
+        self.loopback_btn = QPushButton("Loopback Off")
+        self.loopback_btn.setCheckable(True) 
+        self.loopback_btn.clicked.connect(self.set_loopback)
+
+        self.start_btn = QPushButton('▶️')
+        self.start_btn.setCheckable(True) #when clicked, button.isChecked() = True until clicked again
+        self.start_btn.clicked.connect(self.toggle_scan)
+
+        self.new_scan_btn = QPushButton('↻')
+        self.new_scan_btn.clicked.connect(self.reset_scan)
+        
+
+        mode_options = QGridLayout()
+        mode_options.addWidget(self.conn_btn,0,0)
+        mode_options.addWidget(self.mode_select_dropdown,0,1)
+        mode_options.addWidget(self.loopback_btn,0,3)
+        mode_options.addWidget(self.start_btn,0,4)
+        mode_options.addWidget(self.new_scan_btn,0,5)
+
+
+        self.layout.addLayout(mode_options,0,0)
+
+        self.resolution_options = ResolutionDropdown()
+        self.layout.addLayout(self.resolution_options, 1,0)
+        self.resolution_options.menu.currentIndexChanged.connect(self.changeResolution)
+
+        self.dwell_options = DwellTimeSelector()
+        self.layout.addLayout(self.dwell_options, 1,1)
+
+        self.new_pattern_btn = QPushButton('Pattern file')
+        self.new_pattern_btn.clicked.connect(self.file_select)
+        self.layout.addWidget(self.new_pattern_btn, 1,1)
+        self.new_pattern_btn.setHidden(True)
+
+        self.win = pg.GraphicsLayoutWidget()
+        self.image_view = self.win.addViewBox()
+        ## lock the aspect ratio so pixels are always square
+        self.image_view.setAspectLocked(True)
+        self.image_view.setRange(QtCore.QRectF(0, 0,self.dimension, self.dimension))
+        self.layout.addWidget(self.win)
+
+        self.live_img = pg.ImageItem(border='w', levels = (0,255), axisOrder="row-major")
+        self.image_view.addItem(self.live_img)
+        buf = np.ones((512,512))
+        self.live_img.setImage(buf)
+
+        self.setState("disconnected")
+
+        
+        # self.layout.addWidget(file_dialog)
+
+    def file_select(self):
+        self.file_dialog = ImportPatternFileWindow()
+        self.file_dialog.show()
+        
+
+
     @asyncSlot()
     async def connect(self):
         connection = await scan_controller.connect()
         self.conn_btn.setText(connection)
+        if connection == "Connected":
+            self.setState("scan_not_started")
+    
+    @asyncSlot()
+    async def mode_select(self):
+        await scan_controller.set_mode()
+        mode = self.mode_select_dropdown.currentText()
+        if mode == "Imaging":
+            print("Imaging")
+            self.new_pattern_btn.setHidden(True)
+            self.dwell_options.label.setHidden(False)
+            self.dwell_options.spinbox.setHidden(False)
+        if mode == "Patterning":
+            print("Patterning")
+            self.dwell_options.label.setHidden(True)
+            self.dwell_options.spinbox.setHidden(True)
+            self.new_pattern_btn.setHidden(False)
+            
+            
+
+    @asyncSlot()
+    async def set_loopback(self):
+        await scan_controller.set_loopback()
+        if self.loopback_btn.isChecked():
+            self.loopback_btn.setText("Loopback On")
+        else:
+            self.loopback_btn.setText("Loopback Off")
+        
+
+    @asyncSlot()
+    async def reset_scan(self):
+        await scan_controller.reset_scan()
+
+    @asyncSlot()
+    async def toggle_scan(self):
+        if self.start_btn.isChecked():
+            self.start_btn.setText('🔄')
+            await scan_controller.start_scan()
+            loop = asyncio.get_event_loop()
+            print("GUI", loop)
+            self.update_continously = asyncio.ensure_future(self.keepUpdating())
+            self.setState("scanning")
+            self.start_btn.setText('⏸️')
+
+        else:
+            print("Stopped scanning now")
+            self.start_btn.setText('🔄')
+            await scan_controller.stop_scan()
+            self.start_btn.setText('▶️')
+            self.setState("scan_paused")
+    
+    @asyncSlot()
+    async def changeResolution(self):
+        res_bits = self.resolution_options.menu.currentIndex() + 9 #9 through 14
+        dimension = pow(2,res_bits)
+        await scan_controller.set_resolution(res_bits)
+        buf = np.ones(shape=(dimension,dimension))
+        self.image_view.setRange(QtCore.QRectF(0, 0, dimension, dimension))
+        self.live_img.setImage(buf)
+        print("setting resolution to", dimension)
+
+    async def keepUpdating(self):
+        while True:   
+            try:
+                await self.updateData()
+            except RuntimeError:
+                print("error")
+                break
+    
+    async def updateData(self):
+        await scan_controller.get_single_packet()
+        # scan_controller.stream_to_buffer(data)
+        self.live_img.setImage(scan_controller.buf)
+
+
+
+
+    def setState(self, state):
+        if state == "disconnected":
+            self.start_btn.setEnabled(False)
+            self.new_scan_btn.setEnabled(False)
+            self.loopback_btn.setEnabled(False)
+            self.resolution_options.menu.setEnabled(False)
+            self.dwell_options.spinbox.setEnabled(False)
+            self.mode_select_dropdown.setEnabled(False)
+        if state == "scan_not_started":
+            self.start_btn.setEnabled(True)
+            self.loopback_btn.setEnabled(True)
+            self.resolution_options.menu.setEnabled(True)
+            self.dwell_options.spinbox.setEnabled(True)
+            self.mode_select_dropdown.setEnabled(True)
+        if state == "scanning":
+            self.new_scan_btn.setEnabled(False)
+            self.loopback_btn.setEnabled(False)
+            self.resolution_options.setEnabled(False)
+            self.dwell_options.setEnabled(False)
+        if state == "scan_paused":
+            self.new_scan_btn.setEnabled(True)
+            self.loopback_btn.setEnabled(True)
+            self.resolution_options.setEnabled(True)
+            self.dwell_options.setEnabled(True)
+
+
+
 
 
 
 w = MainWindow()
+
+
+
 
 
 ## https://github.com/CabbageDevelopment/qasync/blob/master/examples/aiohttp_fetch.py
