dbf14390a566fd054007fee4675076728faae354
isabelburgos <isabelmburgos01@gmail.com>
2023-11-26 19:37:35 -0800

    at least it builds

--- software/glasgow/applet/video/scan_gen/scan_gen_components/output_bus.py
+++ software/glasgow/applet/video/scan_gen/scan_gen_components/output_bus.py
@@ -2,7 +2,7 @@ import amaranth
 from amaranth import *
 from amaranth.sim import Simulator
 from amaranth.lib import data, enum
-from amaranth.lib.fifo import SyncFIFO
+from amaranth.lib.fifo import SyncFIFO, SyncFIFOBuffered
 
 if "glasgow" in __name__: ## running as applet
     from ..scan_gen_components.addresses import *
@@ -15,10 +15,11 @@ else:
 
 
 class OutputBus(Elaboratable):
-    def __init__(self, out_fifo, is_simulation):
+    def __init__(self, is_simulation = True, out_fifo = None):
         self.video_sink = VideoSink()
         if is_simulation:
-            self.out_fifo = SyncFIFOBuffered(width = 8, depth = 16, fwft = True)
+            #self.out_fifo = SyncFIFO(width = 8, depth = 16, fwft = True)
+            self.out_fifo = SyncFIFOBuffered(width = 8, depth = 16)
         else:
             self.out_fifo = out_fifo
         #self.pixel_in = Signal(16)
@@ -38,13 +39,14 @@ class OutputBus(Elaboratable):
         with m.FSM() as fsm:
             with m.State("Reading Data"):
                 m.d.sync += self.out_fifo.w_en.eq(0)
-                with m.If(self.strobe):
-                    m.d.comb += self.video_sink.sinking.eq(1)
-                    with m.If(self.video_sink.pipeline_offsetter.pipeline_full):
-                        m.d.sync += self.video_outbox.parsing.eq(1)
-                        m.d.sync += self.out_fifo.w_en.eq(1)
-                        with m.If(self.video_outbox.flag):
-                            m.next = "Writing Second Byte"
+                with m.If(self.out_fifo.w_rdy):
+                    with m.If(self.strobe):
+                        m.d.comb += self.video_sink.sinking.eq(1)
+                        with m.If(self.video_sink.pipeline_offsetter.pipeline_full):
+                            m.d.sync += self.video_outbox.parsing.eq(1)
+                            m.d.sync += self.out_fifo.w_en.eq(1)
+                            with m.If(self.video_outbox.flag):
+                                m.next = "Writing Second Byte"
             with m.State("Writing Second Byte"):
                 m.next = "Reading Data"
 
