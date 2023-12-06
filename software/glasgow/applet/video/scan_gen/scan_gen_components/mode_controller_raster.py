b446f891d4fb0723ba383b0eb6d0b8cb8567804d
isabelburgos <isabelmburgos01@gmail.com>
2023-12-06 13:30:41 -0800

    Add comments

--- software/glasgow/applet/video/scan_gen/scan_gen_components/mode_controller_raster.py
+++ software/glasgow/applet/video/scan_gen/scan_gen_components/mode_controller_raster.py
@@ -17,11 +17,49 @@ else:
 
 
 class RasterOutput(Elaboratable):
+    '''
+    in_fifo_w_data: Signal, out, 8
+        This signal combinatorially drives the top level in_fifo.w_data
+
+    raster_position_data_c: Signal, in, 32:
+        This signal is combinatorially driven by the same value that sets
+        the next x and y position for the beam controller.
+    raster_position_data: Signal, internal, 32
+        When strobe_in_xy is asserted, this signal is synchronously set
+        to the value of raster_position_data_c
+    The position data of each point is stored but not used. This leaves the
+    option of reconfiguring to return X and Y data as well as brightness
+    for points in a raster scan.
+
+    raster_dwell_data_c: Signal, in, 16:
+        This signal is combinatorially driven by ras_mode_ctrl.raster_output_data.
+        This data is driven from the ADC input data, or if testing in loopback, 
+        the beam controller next dwell time
+    raster_position_data: Signal, internal, 16
+        When strobe_in_dwell is asserted, this signal is synchronously set
+        to the value of vector_dwell_data_c
+
+    enable: Signal, in, 1:
+        Asserted when the in_fifo is ready to be written to. This signal is driven by 
+        mode_ctrl.output_enable, which is driven by the top level io_strobe
+    strobe_in_xy: Signal, in, 1
+        Asserted when valid data is present at raster_position_data_c
+        This signal also indicates that the beam controller has recieved new position
+        data, so this module should prepare to recieve new brightness/dwell time data
+
+    strobe_in_dwell: Signal, in, 1
+        Asserted when valid data is present at raster_dwell_data_c
+    strobe_out: Signal, out, 1
+        Asserted when the data at in_fifo_w_data is *not* valid. 
+        If strobe_out is high, data will *not* be written to the in_fifo
+
+    State Machine:
+                    ↓----------------------↑
+        Waiting -> Dwell Waiting -> D1 -> D2
+                    ↳----------------------↑                   
+    '''
     def __init__(self):
-        #self.out_fifo = out_fifo
         self.in_fifo_w_data = Signal(8)
-        self.in_fifo_w_en = Signal()
-        self.in_fifo_w_rdy = Signal()
 
         self.raster_position_data = Signal(vector_position)
         self.raster_position_data_c = Signal(vector_position)
@@ -41,6 +79,7 @@ class RasterOutput(Elaboratable):
             with m.State("Waiting"):
                 m.d.comb += self.strobe_out.eq(1)
                 with m.If(self.strobe_in_xy):
+                    m.d.sync += self.raster_position_data.eq(self.raster_position_data_c)
                     m.next = "Dwell_Waiting"
             with m.State("Dwell_Waiting"):
                 m.d.comb += self.strobe_out.eq(1)
@@ -67,33 +106,59 @@ class RasterOutput(Elaboratable):
 
 class RasterModeController(Elaboratable):
     '''
+    raster_output: See RasterOutput
+        This module takes 32-bit wide raster position data (not used)
+        and 16-bit wide dwell time or brightness data, and disassembles
+        it into bytes to write to the in_fifo
+    
+    raster_point_data: Signal, internal, 48
+        This signal is driven by the x and y values from the xy_scan_gen module,
+        and the dwell time from the dwell register (currently hardcoded).
+        This signal drives the next x, y, and dwell values for the beam controller.
+        The first 32 bits of this signal also drive the raster_position_c
+        value of raster_output. In this way, the x and y position of each 
+        point can be read into the output stream (but are not)
+
+    raster_point_output: Signal, in, 16
+        This signal is driven by the ADC data sampled at each point.
+        In test_mode = data_loopback, beam controller x position is returned.
+
+    beam_controller_end_of_dwell: Signal, in, 1
+        Driven by the beam controller module when the dwell counter has
+        reached the current dwell time
+    beam_controller_start_dwell: Signal, in, 1
+        Driven by the beam controller module when the dwell counter is 0
+
+    beam_controller_next_x_position: Signal, out, 16:
+        Drives beam_controller.next_x_position
+    beam_controller_next_y_position: Signal, out, 16:
+        Drives beam_controller.next_y_position
+    beam_controller_next_dwell: Signal, out, 16:
+        Drives beam_controller.next_dwell
+
+    ### TO DO: Implement raster patterning mode
+        - Create a RasterInput module to read a stream of 16-bit dwell times
+        - Possibly create a RasterFIFO, to ensure there is still data to
+        read from when the out_fifo isn't available, and thus keep the beam moving
     '''
     def __init__(self):
+        self.raster_output = RasterOutput()
+        self.xy_scan_gen = XY_Scan_Gen()
 
-        #self.beam_controller = BeamController()
+        self.raster_point_data = Signal(vector_point)
+        self.raster_point_output = Signal(16)
 
         self.beam_controller_end_of_dwell = Signal()
         self.beam_controller_start_dwell = Signal()
         self.beam_controller_next_x_position = Signal(16)
         self.beam_controller_next_y_position = Signal(16)
         self.beam_controller_next_dwell = Signal(16)
-
-        self.raster_point_data = Signal(vector_point)
-
-        self.raster_point_output = Signal(16)
-
-        self.raster_output = RasterOutput()
-
-        self.xy_scan_gen = XY_Scan_Gen()
-
-       # self.raster_fifo = SyncFIFOBuffered(width = 8, depth = 12)
-
+        # self.raster_fifo = SyncFIFOBuffered(width = 8, depth = 12)
 
     def elaborate(self, platform):
         m = Module()
-        #m.submodules["BeamController"] = self.beam_controller
         m.submodules["RasterOutput"] = self.raster_output
-       # m.submodules["RasterFIFO"] = self.raster_fifo
+        # m.submodules["RasterFIFO"] = self.raster_fifo
         m.submodules["XYScanGen"] = self.xy_scan_gen
 
 
--- software/glasgow/applet/video/scan_gen/scan_gen_components/mode_controller_vector.py
+++ software/glasgow/applet/video/scan_gen/scan_gen_components/mode_controller_vector.py
@@ -18,13 +18,45 @@ else:
 
 
 class VectorInput(Elaboratable): 
+    '''
+    out_fifo_r_data: Signal, in, 8
+        This signal is combinatorially driven by the top level out_fifo.r_data
+    
+    vector_point_data: Signal, internal, 48
+        Each cycle that the out_fifo is read from, a byte is synchronously written 
+        to vector_point_data. 
+    vector_point_data_c: Signal, out, 48
+        This is the signal that the next values for the beam controller are read from.
+        This signal is combinatorially assigned to the value of vector_point_data
+        and the latest byte from the out_fifo. This is so that the full vector point
+        data can be used immediately. If the data can't be used immediately, the last
+        byte is synchronously added to vector_point_data and the state machine moves to 
+        the HOLD state.
+
+    data_complete: Signal, out, 1:
+        Asserted when all 6 bytes of a vector point have been assembled. When this is true,
+        the value of vector_point_c is valid to assign as the next beam controller values
+    enable: Signal, in, 1:
+        Asserted when the out_fifo is ready to be read from. This signal is driven by 
+        mode_ctrl.output_enable, which is driven by the top level io_strobe
+    strobe_out: Signal, in, 1:
+        Asserted when the data held in vector_point_data is used. On the cycle after this is
+        asserted, the module will return to state X1 and be ready to read a new point in
+
+    State Machine:
+        X1 -> X2 -> Y1 -> Y2 -> D1 -> D2 -> Hold
+                                    X1 ↲    X1 ↲
+
+    Inspiration taken from https://github.com/maia-sdr/maia-sdr/blob/main/maia-hdl/maia_hdl/packer.py
+    
+    '''
     def __init__(self):
         self.out_fifo_r_data = Signal(8)
 
         self.vector_point_data = Signal(vector_point)
         self.vector_point_data_c = Signal(vector_point)
+        
         self.data_complete = Signal()
-
         self.enable = Signal()
         self.strobe_out = Signal()
 
@@ -76,6 +108,40 @@ class VectorInput(Elaboratable):
 
 
 class VectorOutput(Elaboratable):
+    '''
+    in_fifo_w_data: Signal, out, 8
+        This signal combinatorially drives the top level in_fifo.w_data
+
+    vector_position_data_c: Signal, in, 32:
+        This signal is combinatorially driven by the same value that sets
+        the next x and y position for the beam controller.
+    vector_position_data: Signal, internal, 32
+        When strobe_in_xy is asserted, this signal is synchronously set
+        to the value of vector_position_data_c
+    vector_dwell_data_c: Signal, in, 16:
+        This signal is combinatorially driven by vec_mode_ctrl.vector_output_data.
+        This data is driven from the ADC input data, or if testing in loopback, 
+        the beam controller next dwell time
+    vector_position_data: Signal, internal, 16
+        When strobe_in_dwell is asserted, this signal is synchronously set
+        to the value of vector_dwell_data_c
+
+    enable: Signal, in, 1:
+        Asserted when the in_fifo is ready to be written to. This signal is driven by 
+        mode_ctrl.output_enable, which is driven by the top level io_strobe
+    strobe_in_xy: Signal, in, 1
+        Asserted when valid data is present at vector_position_data_c
+    strobe_in_dwell: Signal, in, 1
+        Asserted when valid data is present at vector_dwell_data_c
+    strobe_out: Signal, out, 1
+        Asserted when the data at in_fifo_w_data is *not* valid. 
+        If strobe_out is high, data will *not* be written to the in_fifo
+
+    State Machine:
+            ↓------------------------------------------------------↑
+        Waiting -> X1 -> X2 -> Y1 -> Y2 -> Dwell Waiting -> D1 -> D2
+            ↳------------↑                       ↳-----------------↑
+    '''
     def __init__(self):
         self.in_fifo_w_data = Signal(8)
 
@@ -142,17 +208,48 @@ class VectorOutput(Elaboratable):
 
 class VectorModeController(Elaboratable):
     '''
+    vector_fifo: FIFO, 48 bits wide, 12 values deep
+        This FIFO holds full vector points
+
+    vector_input: See VectorInput
+        This module handles reading data from the out_fifo and 
+        assembling into 48-bit vector point format
+    vector_output: See VectorOutput
+        This module takes 32-bit wide vector position data and
+        16-bit wide dwell time or brightness data, and disassembles
+        it into bytes to write to the in_fifo
+    
+    vector_point_data: Signal, internal, 48
+        This signal is driven by the value read from the vector FIFO,
+        and drives the next x, y, and dwell values for the beam controller.
+        The first 32 bits of this signal also drive the vector_position_c
+        value of vector_output. In this way, the x and y position of each 
+        point are read into the output stream
+
+    vector_point_output: Signal, in, 16
+        This signal is driven by the ADC data sampled at each point.
+        In test_mode = data_loopback, the dwell time is returned directly.
+
+    beam_controller_end_of_dwell: Signal, in, 1
+        Driven by the beam controller module when the dwell counter has
+        reached the current dwell time
+    beam_controller_start_dwell: Signal, in, 1
+        Driven by the beam controller module when the dwell counter is 0
+
+    beam_controller_next_x_position: Signal, out, 16:
+        Drives beam_controller.next_x_position
+    beam_controller_next_y_position: Signal, out, 16:
+        Drives beam_controller.next_y_position
+    beam_controller_next_dwell: Signal, out, 16:
+        Drives beam_controller.next_dwell
     '''
     def __init__(self):
-        #self.beam_controller = BeamController()
-
-        self.vector_point_data = Signal(vector_point)
-
         self.vector_fifo = SyncFIFOBuffered(width = 48, depth = 12)
 
         self.vector_input = VectorInput()
         self.vector_output = VectorOutput()
 
+        self.vector_point_data = Signal(vector_point)
         self.vector_point_output = Signal(16)
 
         self.beam_controller_end_of_dwell = Signal()
