cc69e5957087d3b790bb57a93f019d7ef3759f8f
isabelburgos <isabelmburgos01@gmail.com>
2023-12-07 16:44:49 -0800

    start integrating prev GUIs/streaming modalities

---
+++ software/glasgow/applet/video/scan_gen/output_formats/local_gui.py
@@ -0,0 +1,66 @@
+from time import perf_counter
+
+import numpy as np
+import os
+
+import pyqtgraph as pg
+from pyqtgraph.Qt import QtCore
+
+from PyQt6.QtWidgets import (QMainWindow, QPushButton, 
+                            QGridLayout, QWidget, QComboBox, 
+                            QLabel, QSpinBox, QFileDialog)
+
+from even_newer_gui import ImageDisplay
+
+
+
+class MainWindow(QWidget):
+    def __init__(self, path):
+        super().__init__()
+        self.path = path
+        self.layout = QGridLayout()
+        self.setLayout(self.layout)
+        
+        self.image_display = ImageDisplay(2048, 2048)
+        self.layout.addWidget(self.image_display, 1, 0)
+
+        self.updateTime = perf_counter()
+        elapsed = 0
+
+        self.timer = QtCore.QTimer()
+        self.timer.setSingleShot(True)
+        # not using QTimer.singleShot() because of persistence on PyQt. see PR #1605
+    
+    def get_dimensions(self):
+        settings = np.loadtxt(os.path.join(self.path, "current_display_setting"))
+        dimension = int(settings)
+        return dimension
+    def get_buffer(self):
+        FrameBufDirectory = os.path.join(self.path, "current_frame")
+
+    def updateData(self):
+        d = np.memmap(self.FrameBufDirectory, shape = (dimension*dimension),mode = "r+")
+        data = np.reshape(d,(dimension,dimension))
+        #data = np.random.rand(dimension,dimension)
+
+        print(data)
+        print(data.shape)
+        self.image_display.live_img.setImage(data) #this is the correct orientation to display the image
+        
+        self.timer.start(1)
+
+    def startUpdating(self):
+        self.timer.timeout.connect(self.updateData)
+        self.updateData()
+
+
+def run_gui(path):
+    app = pg.mkQApp("Scan Live View")
+    w = MainWindow(path)
+    w.show()
+    pg.exec()
+
+
+if __name__ == '__main__':
+    run_gui(os.getcwd())
+
