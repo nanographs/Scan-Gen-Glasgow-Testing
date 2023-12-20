aa3d86bfa0ec561457bf010fce5823b0133b466b
isabelburgos <isabelmburgos01@gmail.com>
2023-12-20 12:28:08 -0800

    matching with a regex

--- software/glasgow/applet/video/scan_gen/output_formats/encoding_test.py
+++ software/glasgow/applet/video/scan_gen/output_formats/encoding_test.py
@@ -1,12 +1,29 @@
-data = [255, 0, 255, 0, 3, 0]*16384 + [80] + [255, 0, 255, 0, 3, 0]*16384 + [80] + [255, 0, 255, 0, 3, 0]*16384
+import re
+
+data = [255, 255, 255, 0, 255, 0, 3, 1, 255, 255] + [255, 0, 255, 0, 3, 0]*100 + [255, 255, 255, 1, 255, 1, 3, 1, 255, 255] 
 
 d = bytes(data)
 print(type(d))
 
-print(80 in d)
-print(d.find(80))
+sep = bytes([255,255])
 
-l = d.partition(bytes([80]))
-print(l)
+#print(d.find(sep))
+
+
+pattern = re.compile(b'\xff{2}.{6}\xff{2}')
+n = re.finditer(pattern, d)
+prev_stop = 0
+while True:
+    try:
+        match = next(n)
+        start, stop = match.span()
+        config = match.group()
+        data = d[prev_stop:start]
+        prev_stop = stop
+    except StopIteration:
+        break
+
+# l = d.partition(bytes([80]))
+# print(l)
 
 e = memoryview(d)
